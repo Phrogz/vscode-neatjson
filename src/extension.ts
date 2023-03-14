1e0514fbb7e524d0902c9ce414ad3b3ec5ccced4
Gavin Kistner <gkistner@nvidia.com>
2023-03-14 15:44:34 -0600

    Initial commit - unpolished, mostly working

---
+++ src/extension.ts
@@ -0,0 +1,124 @@
+/* eslint-disable curly */
+// @ts-ignore
+import {neatJSON} from 'neatjson';
+import * as vscode from 'vscode';
+
+interface NeatJSONOptions {
+	wrap?:              number | boolean,
+	indent?:            string,
+	indentLast?:        boolean,
+	short?:             boolean,
+	sort?:              boolean,
+	aligned?:           boolean,
+	decimals?:          false | number,
+	trimTrailingZeros?: boolean,
+	forceFloats?:       boolean,
+	forceFloatsIn?:     string[],
+	arrayPadding?:      number,
+	objectPadding?:     number,
+	padding?:           number,
+	beforeComma?:       number,
+	afterComma?:        number,
+	aroundComma?:       number,
+	beforeColon1?:      number,
+	afterColon1?:       number,
+	beforeColonN?:      number,
+	afterColonN?:       number,
+	beforeColon?:       number,
+	afterColon?:        number,
+	aroundColon?:       number,
+}
+
+let activeFormatter: NeatJSONOptions;
+export function activate(context: vscode.ExtensionContext) {
+	activeFormatter = formatterFromDefaultOptions();
+
+	context.subscriptions.push(vscode.commands.registerCommand('neatJSON.formatWith', selectFormatter));
+	context.subscriptions.push(vscode.commands.registerCommand('neatJSON.formatDocument', formatDocument));
+	context.subscriptions.push(vscode.commands.registerCommand('neatJSON.formatSelection', formatSelection));
+}
+
+async function selectFormatter() {
+	const conf = vscode.workspace.getConfiguration('neatJSON');
+	const formatters : any = conf.get('formatters') || {};
+	const names = Object.keys(formatters);
+	names.unshift('(custom values from settings)');
+	let name = await vscode.window.showQuickPick(names);
+	if (name !== undefined) {
+		activeFormatter = formatters[name] || formatterFromDefaultOptions();
+		console.info(`neatJSON Settings`, activeFormatter);
+
+		const hasSelection = vscode.window.activeTextEditor && !vscode.window.activeTextEditor.selection.isEmpty;
+		if (hasSelection) formatSelection();
+		else              formatDocument();
+	}
+}
+
+function formatDocument() {
+	const editor = vscode.window.activeTextEditor;
+	const doc = editor?.document;
+	const orig = doc?.getText();
+	if (editor && doc && orig) {
+		const formatted = format(orig);
+		if (formatted !== undefined) {
+			const fullRange = doc.validateRange(new vscode.Range(0, 0, doc.lineCount, 0));
+			editor.edit(edit => edit.replace(fullRange, formatted));
+			}
+	}
+}
+
+function formatSelection() {
+	const editor = vscode.window.activeTextEditor;
+	const selection = editor?.selection;
+	if (selection && !selection.isEmpty) {
+		const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+		const selectedText = editor.document.getText(selectionRange);
+		const formatted = format(selectedText);
+		if (formatted !== undefined) editor.edit(edit => edit.replace(selectionRange, formatted));
+	}
+}
+
+function format(json: string) : string | undefined {
+	try {
+		const value = JSON.parse(json);
+		return neatJSON(value, activeFormatter);
+	} catch (e) {
+		console.log({error:e});
+		vscode.window.showWarningMessage('Does not parse as JSON.');
+	}
+}
+
+function formatterFromDefaultOptions() : NeatJSONOptions {
+	const conf = vscode.workspace.getConfiguration('neatJSON');
+	const format:NeatJSONOptions = {};
+	if (!conf.get('wrapLines')) format.wrap = false;
+	else {
+		format.wrap       = conf.get('wrapLines.wrapWidth');
+		format.indent     = (conf.get('indent.usingTabs') ? '\t' : ' ').repeat(conf.get('indent.amount') || 0);
+		format.indentLast = conf.get('indent.indentLast');
+		format.short      = conf.get('short');
+		format.sort       = conf.get('sort');
+		format.aligned    = conf.get('alignColons');
+	}
+	format.arrayPadding  = conf.get('arrayPadding');
+	format.objectPadding = conf.get('objectPadding');
+	format.beforeComma   = conf.get('spacesBeforeCommas');
+	format.afterComma    = conf.get('spacesAfterCommas');
+	format.beforeColon1  = conf.get('spacesBeforeOneLineColons');
+	format.afterColon1   = conf.get('spacesAfterOneLineColons');
+	format.beforeColonN  = conf.get('spacesBeforeMultilineColons');
+	format.afterColonN   = conf.get('spacesAfterMultilineColons');
+
+	format.forceFloats   = conf.get('forceFloats');
+	if (conf.get('forceFloatsIn')) format.forceFloatsIn = conf.get('forceFloatsIn');
+
+	if (conf.get('formatDecimals')) {
+		format.decimals          = conf.get('formatDecimals.precision');
+		format.trimTrailingZeros = conf.get('formatDecimals.trimTrailingZeros');
+	}
+
+	return format;
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
